9564df929e8e62f2a630a3efda7344fb56e38557
yotaro.takagi <test@co.jp>
2020-09-16 10:59:21 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,14 @@
+<header class="mb-4">
+    <nav class="navbar nabvar-expand-sm navbar-dark bg-dark">
+        <a class="nabvar-brand" href="/">Menu</a>
+        
+        <button type="submit" class="nabvar-toggler" data-toggle="collapse" data-target="#navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <li class="nav-item"><a href="#" class="nav-link">SignUp</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">LogIn</a></li>
+        </div>
+    </nav>
+</header>

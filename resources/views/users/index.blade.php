ab62cbef9f21b48f8f76310fe32f8d70c3608ebe
yotaro.takagi <test@co.jp>
2020-11-11 01:13:45 +0000

    Procfile

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,14 +8,7 @@
        <p class="plofile_picture">
            <img src="storage/avatar/IMG_0058.jpeg" width="150px" height="150px" alt="プロフィール写真が選択されていません">
        </p>
-       
-        <!--{!! Form::open(["route" => "fileUpload", 'enctype'=>'multipart/form-data']) !!}
-                
-            {!! Form::label("avatar", "プロフィール写真") !!}
-            {!! Form::file("avatar", ["files" => true, "class" => "form-control-file"]) !!}
-            {!! Form::submit("アップロード", ["class" => "btn btn-primary btn-block"]) !!}
-                
-        {!! Form::close() !!}-->
+      
     </div>
     
     <div>
@@ -36,17 +29,6 @@
                 {!! Form::label("timefix", "釣った時間") !!}
                 {!! Form::text("timefix", null, ["class" => "form-control"]) !!}
             </div>
-            
-            <!--<div class="form-group">
-                {!! Form::label("fish_picture", "写真") !!}
-                {!! Form::file("fish_picture", ["files" => true, "class" => "form-control-file"]) !!}
-            </div>-->
-            
-            <!--<div class="form-group">
-                {!! Form::label("place", "釣った場所") !!}
-                {!! Form::text("place", null, ["class" => "form-control"]) !!}
-            </div>-->
-            
             
             
             {!! Form::submit("投稿する", ["class" => "btn btn-primary btn-block"]) !!}
